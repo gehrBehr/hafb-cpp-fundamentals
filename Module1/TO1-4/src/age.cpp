c0d3cebc1c0a397aa54c855c9ab17fba0429744c
CCEClass1 <gehringadam@byui.edu>
2019-12-17 11:48:44 -0700

    Added code to the age.cpp and age.h files.

--- Module1/TO1-4/src/age.cpp
+++ Module1/TO1-4/src/age.cpp
@@ -1,1 +1,18 @@
 #include "age.h"
+
+
+void ByReferenceAgeIn5Years(int& age)
+{
+    age += 5;
+}
+
+void ByValueAgeIn5Years(int age)
+{
+    age += 5;
+}
+
+
+int CurrentAge(int age)
+{
+    return age;
+}
--- Module1/TO1-4/src/age.h
+++ Module1/TO1-4/src/age.h
@@ -1,5 +1,12 @@
 #ifndef AGE_H_
 #define AGE_H_
 
+#include <iostream>
+using namespace std;
+
+//Prototypes
+void ByValueAgeIn5Years(int);
+void ByReferenceAgeIn5Years(int&);
+int CurrentAge(int);
 
 #endif /* !AGE_H_ */
