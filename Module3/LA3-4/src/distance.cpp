a999ed44a2aa5e88f64c45050e1feb1e2fb3ca94
CCEClass1 <gehringadam@byui.edu>
2019-12-19 11:16:38 -0700

    created comparison overload operators.

--- Module3/LA3-4/src/distance.cpp
+++ Module3/LA3-4/src/distance.cpp
@@ -1,6 +1,83 @@
-// countpp3.cpp
-// increment counter variable with ++ operator
-// uses unnamed temporary object
-#include <iostream>
-using namespace std;
+
 #include "distance.h"
+
+void Distance::ShowDist() const
+{
+    std::cout << feet() << "\'- " << inches() << "\"" << std::endl;
+}
+
+Distance Distance::operator + (Distance rhs) const
+{
+    int feet = feet_ + rhs.feet_;
+    float inches = inches_ + rhs.inches_;
+
+    feet += (inches / 12); 
+    inches = fmod(inches, 12.0);
+
+    Distance temp(feet, inches);
+    return temp;
+}
+
+Distance Distance::operator - (Distance rhs) const
+{
+    float rhsInches = rhs.inches_ + (rhs.feet_ * 12.0);
+
+    float lhsInches = inches_ + (feet_ * 12.0);
+
+    float temp = lhsInches - rhsInches;
+
+    float inches = fmod(temp, 12.0);
+
+    int feet = temp / 12; 
+
+    return Distance(feet, inches);
+}
+
+bool Distance::operator < (Distance rhs) const
+{
+    if (feet_ < rhs.feet_ )
+    {
+        return true;
+    }
+    else
+    {
+        return (((feet_* 12.0) + inches_) < ((rhs.feet_ * 12.0) + rhs.inches_) );
+    }
+}
+
+bool Distance::operator > (Distance rhs) const
+{
+    if (feet_ > rhs.feet_ )
+    {
+        return true;
+    }
+    else
+    {
+        return (((feet_* 12.0) + inches_) > ((rhs.feet_ * 12.0) + rhs.inches_) );
+    }
+}
+
+bool Distance::operator == (Distance rhs) const
+{
+    return (((feet_* 12.0) + inches_) == ((rhs.feet_ * 12.0) + rhs.inches_) );
+}
+
+bool Distance::operator != (Distance rhs) const
+{
+    return (((feet_* 12.0) + inches_) != ((rhs.feet_ * 12.0) + rhs.inches_) );
+}
+
+
+std::ostream& operator <<(std::ostream& os, const Distance& distance)
+{
+    //Do Not include an endline character. 
+    os << distance.feet_ << "ft " << distance.inches_ << "in " ;
+    return os;
+}
+
+void Distance::update_distance(int ft, float in)
+{
+    set_feet(ft);
+    set_inches(in);
+}
+
---
+++ Module3/LA3-4/src/distance.h
@@ -0,0 +1,50 @@
+/*
+** EPITECH PROJECT, 2019
+** hafb-cpp-fundamentals
+** File description:
+** distance
+*/
+
+#ifndef DISTANCE_H_
+#define DISTANCE_H_
+#include <iostream>
+#include <array>
+#include <math.h>
+
+class Distance
+{
+private:
+    float inches_;
+    int feet_;
+    double miles_;
+public:
+    Distance() : feet_(0), inches_(0){}
+    Distance(int feet, float inches) : feet_(feet), inches_(inches){}
+
+    ~Distance(){}
+    //Getters and Setters.
+    int feet()const {return feet_;}
+    float inches()const {return inches_;}
+
+    void set_feet(int feet){feet_ = feet;}
+    void set_inches(float inches){inches_ = inches;}
+    // other methods
+    void ShowDist() const;
+    Distance operator + (Distance rhs) const;
+    friend std::ostream& operator <<(std::ostream& os, const Distance& distance); //return a reference to an output stream.
+    Distance operator - (Distance rhs) const;
+
+    void update_distance(int ft, float in);
+    //compare objects
+    bool operator < (Distance rhs) const; 
+    bool operator > (Distance rhs) const; 
+    
+    bool operator == (Distance rhs) const; 
+    bool operator != (Distance rhs) const; 
+
+
+
+
+};
+
+#endif /* !DISTANCE_H_ */
